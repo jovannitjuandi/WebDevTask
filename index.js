0f5da1e6a1908c7a2c653a3d07184a4931524864
jovannitjuandi <jovanni.tjuandi@gmail.com>
2020-11-24 13:35:19 +1100

    set up server

---
+++ index.js
@@ -0,0 +1,50 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+	let filePath = path.join(__dirname, 'Pages', req.url === '/' ? 'index.html' : req.url);
+	let ext = path.extname(filePath);
+	let contentType = 'text/html';
+	
+	switch(ext) {
+		case '.js':
+			contentType = 'text/javascript';
+			break;
+		case '.css':
+			contentType = 'text/css';
+			break;
+		case '.json':
+			contentType = 'application/json';
+			break;
+		case '.png':
+			contentType = 'image/png';
+			break;
+		case '.jpg':
+			contentType = 'image/jpg';
+			break;
+		default: {
+			contentType = 'text/html';
+		}
+	}
+	
+	fs.readFile(filePath, (err, content) => {
+		if (err) {
+			if (err.code == 'ENOENT') {
+				fs.readFile(path.join(__dirname, 'Pages', 'error.html'), (err, content) => {
+					res.writeHead(200, {'Content-Type': 'text/html'});
+					res.end(content, 'utf8');
+				})
+			} else {
+				res.writeHead(500);
+				res.end(`SERVER ERROR: ${err.code}`)
+			}
+		} else {
+			res.writeHead(200, {'Content-Type': contentType})
+			res.end(content, 'utf8');
+		}
+	});
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server Running on ${PORT}`));
